35300a40f9e485de6999d23a974ba2a8d84acfcd
muthomi254 <dmunene202020@gmail.com>
2023-10-19 14:38:01 +0300

    fix: server

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 
+const { JSDOM } = require('jsdom');
+
+// Create a fake DOM environment
+const dom = new JSDOM();
+global.document = dom.window.document;
 
 
 document.addEventListener('DOMContentLoaded', function () {
